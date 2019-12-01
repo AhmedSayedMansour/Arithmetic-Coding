ae4ad3537b357413d60a0d0e9e3d0cd13f63850f
Ahmed Sayed Mansour <ahmed.mans20719@gmail.com>
2019-12-02 01:07:09 +0200

    initial commit

---
+++ src/arithmetic/coding/ArithmeticCoding.java
@@ -0,0 +1,78 @@
+package arithmetic.coding;
+
+import java.util.*;
+
+public class ArithmeticCoding {
+    public static HashMap<Character, Double> rangeLow  = new HashMap<Character, Double>();
+    public static HashMap<Character, Double> rangeHigh = new HashMap<Character, Double>();
+    public static char lastChar = ' ';
+    
+    public static Double compress(String s){
+        Double Lower = 0.0 , Upper = 1.0 , originLower;
+        for(int i=0 ; i<s.length() ; ++i){
+            originLower = Lower;
+            Lower = originLower + (Upper-originLower) * rangeLow.get(s.charAt(i));
+            Upper = originLower + (Upper-originLower) * rangeHigh.get(s.charAt(i));
+        }
+        return nearest(Lower, Upper);
+    }
+    
+    public static String decompress(Double code, int numSym){
+        Double Lower = 0.0 , Upper = 1.0 , newCode = code;
+        String s="";
+        for(int i=0 ; i<numSym ; ++i){
+            for (Character c : rangeLow.keySet()) {
+                if(newCode>rangeLow.get(c) && newCode<rangeHigh.get(c)){
+                    s+=c;
+                    Lower = Lower + (Upper-Lower) * rangeLow.get(c);
+                    Upper = Lower + (Upper-Lower) * rangeHigh.get(c);
+                    newCode = (code - Lower)/(Upper - Lower);
+                    break;
+                }
+            }
+        }
+        return s;
+    }
+    
+    public static Double nearest(Double low , Double high){
+        Double num = 0.0;
+        Double i = -1.0;
+        while(!(num >low && num < high)){
+            num+=Math.pow(2, i);
+            if(num > high){
+                num-=Math.pow(2, i);
+            }
+            --i;
+        }
+        return num;
+    }
+    
+    public static void addRange(Character c,Double range){
+        if(rangeLow.size() == 0){
+            rangeLow.put(c,0.0);
+            rangeHigh.put(c,range);
+        }
+        else{
+            rangeLow.put(c, rangeHigh.get(lastChar));
+            rangeHigh.put(c, rangeHigh.get(lastChar) + range);
+        }
+        lastChar = c;
+    }
+    
+    public static void main(String[] args) {
+        
+        new GUI().setVisible(true);
+        
+        /*tests*/
+        /*
+        rangeLow.put('a',0.0);
+        rangeHigh.put('a',0.8);
+        rangeLow.put('b',0.8);
+        rangeHigh.put('b',0.82 );
+        rangeLow.put('c',0.82);
+        rangeHigh.put('c',1.0);
+        System.out.println(compress("acba"));
+        System.out.println(decompress(compress("acba") , 4));
+        */
+    }
+}
